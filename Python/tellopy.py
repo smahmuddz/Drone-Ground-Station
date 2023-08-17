7cd177aae33ce07e155ab70ec78b1246f17b8a9e
Sabbir-Mahmud-Afridi <smahmuddz@gmail.com>
2023-08-17 20:50:58 +0600

    Updated tellopy

--- Python/tellopy.py
+++ Python/tellopy.py
@@ -1,17 +1,16 @@
-from djitellopy import tello
+from djitellopy import Tello
 from time import sleep
 import cv2
 
-myDrone = tello.Tello()
+myDrone = Tello()
 myDrone.connect()
-myDrone.stream_on()
+myDrone.streamon()  # Use streamon() instead of stream_on()
 
-print("Battery: " + myDrone.get_battery())
-print("Height: " + myDrone.get_height())
-print("Temprature: " + myDrone.get_temperature())
-print("Speed (X) : " + myDrone.get_speed_x())
-print("Speed (y) : " + myDrone.get_speed_y())
-
+print("Battery: " + str(myDrone.get_battery()))  # Convert battery value to string
+print("Height: " + str(myDrone.get_height()))    # Convert height value to string
+print("Temperature: " + str(myDrone.get_temperature()))  # Corrected 'Temprature' to 'Temperature'
+print("Speed (X): " + str(myDrone.get_speed_x()))  # Convert speed_x value to string
+print("Speed (Y): " + str(myDrone.get_speed_y()))  # Convert speed_y value to string
 
 # here parameter is in cm.
 # myDrone.takeoff()
@@ -22,14 +21,16 @@ print("Speed (y) : " + myDrone.get_speed_y())
 # myDrone.move_right(100)
 # myDrone.land()
 
-
 # Get Video
 while True:
     img = myDrone.get_frame_read().frame
-    img = cv2.resize(370,240)
-    cv2.imshow("Image",img)
-    cv2.waitKey(1)
+    img = cv2.resize(img, (640, 480))  # Corrected the resize parameters
+    cv2.imshow("Image", img)
+    if cv2.waitKey(1) & 0xFF == ord('q'):  # Exit loop when 'q' key is pressed
+        break
 
+myDrone.streamoff()  # Use streamoff() instead of stream_on()
+cv2.destroyAllWindows()
 
-# DJI Tello drone python interface using the official Tello SDK and Tello EDU SDK.  
+# DJI Tello drone python interface using the official Tello SDK and Tello EDU SDK.
 # https://github.com/damiafuentes/DJITelloPy

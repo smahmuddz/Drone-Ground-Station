05d5374fc5774d762a4823c551add53516dbb8d2
Sabbir-Mahmud-Afridi <smahmuddz@gmail.com>
2023-08-18 21:01:02 +0600

    Incomplete Control server

--- Control_server.py
+++ Control_server.py
@@ -1,25 +1,40 @@
-from flask import Flask, Response
+from flask import Flask, Response, request
 from djitellopy import Tello
 import cv2
-from time import *
+import threading
+
 app = Flask(__name__)
-# myDrone = Tello()
-# myDrone.connect()
+myDrone = Tello()
+myDrone.connect()
+myDrone.streamon()
+
+# Flag to control the streaming loop
+streaming = True
+
+def generate_frames():
+    while streaming:
+        img = myDrone.get_frame_read().frame
+        img = cv2.resize(img, (640, 480))
+        _, buffer = cv2.imencode('.jpg', img)
+        frame = buffer.tobytes()
+        yield (b'--frame\r\n'
+               b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
 
 @app.route('/')
 def index():
-    return open("index.html").read()
+    return Response(generate_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
 
-@app.route('/takeoff', methods=['get'])
-def takeoff():
-    print("Takeoff Button Pressed")
-    sleep(4)  # Adjust sleep duration as needed
-    return "take off"
+@app.route('/send-command', methods=['POST'])
+def send_command():
+    try:
+        command = request.form.get('command')
+        # Process the command here
+        print(command)
+        response_data = "Command received: " + command
+        return Response(response_data, content_type='text/plain; charset=UTF-8')
+    except Exception as e:
+        return Response("Error processing command: " + str(e), content_type='text/plain; charset=UTF-8')
 
-@app.route('/land', methods=['get'])
-def land():
-    print("Land Button Pressed")
-    return "Land"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
